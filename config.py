5d3e14d68d257ae14b15af1f07e089fa4761b5d2
dnaclub <dnaclub@yahoo.com>
2025-06-26 00:42:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+TOKEN = "8080558645:AAEOMT4XeXtiK2Tj9AmxL9rLCMjE5l4Y-P8"
+ADMIN_USERNAME = "professorbetts"
+INVITE_LINK = "https://t.me/+iWQnIJAlMUoyZTg0"
+PAYMENT_MESSAGE = (
+    "💳 Για να αποκτήσεις πρόσβαση στο κανάλι:
+
+"
+    "🔹 Πλήρωσε 30€ με Revolut: https://revolut.me/anaariadnan
+"
+    "🔹 Ή στείλε κωδικό Paysafe
+
+"
+    "📸 Μετά την πληρωμή, στείλε screenshot εδώ για έγκριση."
+)
