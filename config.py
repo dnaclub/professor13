72ae401558deedad7245174068bdefbeaf211f02
dnaclub <dnaclub@yahoo.com>
2025-06-26 00:40:49 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = "8080558645:AAEOMT4XeXtiK2Tj9AmxL9rLCMjE5l4Y-P8"
-ADMIN_USERNAME = "professorbetts"
-INVITE_LINK = "https://t.me/+iWQnIJAlMUoyZTg0"
-PAYMENT_MESSAGE = (
-    "💳 Για να αποκτήσεις πρόσβαση στο κανάλι:\n\n"
-    "🔹 Πλήρωσε 30€ με Revolut: https://revolut.me/anaariadnan\n"
-    "🔹 Ή στείλε κωδικό Paysafe\n\n"
-    "📸 Μετά την πληρωμή, στείλε screenshot εδώ για έγκριση."
-)
