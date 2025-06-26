afd5a66e051f255d99f2f6e4f9c557398497d5ec
dnaclub <dnaclub@yahoo.com>
2025-06-26 14:47:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "8080558645:AAEOMT4XeXtiK2Tj9AmxL9rLCMjE5l4Y-P8"
-ADMIN_USERNAME = "7189542392"
+ADMIN_USER_ID = 7189542392
 INVITE_LINK = "https://t.me/+iWQnIJAlMUoyZTg0"
 PAYMENT_MESSAGE = (
     "💳 Για να αποκτήσεις πρόσβαση στο κανάλι:\n\n"
